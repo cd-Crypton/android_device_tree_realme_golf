fe27483f08cf740da6f883930fee1a6e6ac8778f
luk1337 <priv.luk@gmail.com>
2023-03-08 11:15:41 +0800

    golf: Import sensors/2.0/multihal.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -21,6 +21,14 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0-service-multihal.golf \
+    libsensorndkbridge
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service-oplus
